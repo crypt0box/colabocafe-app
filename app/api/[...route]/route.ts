dfc11bdbb3a3e8d11f9fe2351df24878a2642250
crypt0box <pray.happy76@gmail.com>
2024-09-16 13:29:44 +0900

    chore: install biome

--- app/api/[...route]/route.ts
+++ app/api/[...route]/route.ts
@@ -1,18 +1,20 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
 
-export const runtime = 'edge'
+export const runtime = "edge";
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath("/api");
 
-app.get('/hello', async(c) => {
-  const rss = await fetch('https://news.google.com/rss/search?q=コラボカフェ').then((res) => {
-    return res.text();
-  })
-  console.log(rss)
-  return c.json({
-    message: 'Hello from Hono!'
-  })
-})
+app.get("/hello", async (c) => {
+	const rss = await fetch(
+		"https://news.google.com/rss/search?q=コラボカフェ",
+	).then((res) => {
+		return res.text();
+	});
+	console.log(rss);
+	return c.json({
+		message: "Hello from Hono!",
+	});
+});
 
-export const GET = handle(app)
+export const GET = handle(app);
