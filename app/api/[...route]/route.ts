a6a6b1eb2f8fe63692c431b13dc698cee738e456
M-Taguchi <ponpocori.chiccyorina@gmail.com>
2024-09-16 13:04:26 +0900

    rss取得テスト

--- app/api/[...route]/route.ts
+++ app/api/[...route]/route.ts
@@ -5,7 +5,11 @@ export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.get('/hello', (c) => {
+app.get('/hello', async(c) => {
+  const rss = await fetch('https://news.google.com/rss/search?q=コラボカフェ').then((res) => {
+    return res.text();
+  })
+  console.log(rss)
   return c.json({
     message: 'Hello from Hono!'
   })
