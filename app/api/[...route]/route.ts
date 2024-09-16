3448d9deae169d2765d90e4a15d3d9fd8c65ffc2
cryptobox <pray.happy76@gmail.com>
2024-09-16 16:55:40 +0900

    chore: 設定を追記

--- app/api/[...route]/route.ts
+++ app/api/[...route]/route.ts
@@ -1,37 +1,70 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
-import { XMLParser } from 'fast-xml-parser'
+import { D1Database } from "@cloudflare/workers-types";
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { XMLParser } from "fast-xml-parser";
 
-export const runtime = 'edge'
+export const runtime = "edge";
 
-const app = new Hono().basePath('/api')
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      DB: D1Database;
+    }
+  }
+}
 
-app.get('/hello', async(c) => {
+type Bindings = {
+  DB: D1Database;
+};
+
+const app = new Hono<{ Bindings: Bindings }>().basePath("/api");
+
+app.get("/hello", async (c) => {
   const res: {
-    id: string,
-    title: string,
-    link: string,
-    publicationDate: string,
-    description: string,
-    source: string
+    id: string;
+    title: string;
+    link: string;
+    publicationDate: string;
+    description: string;
+    source: string;
   }[] = [];
   const parser = new XMLParser();
-  const dom = parser.parse(await (await fetch('https://news.google.com/rss/search?q=コラボカフェ&hl=ja&gl=JP&ceid=JP:ja')).text());
+  const dom = parser.parse(
+    await (
+      await fetch(
+        "https://news.google.com/rss/search?q=コラボカフェ&hl=ja&gl=JP&ceid=JP:ja"
+      )
+    ).text()
+  );
   const items = dom.rss.channel.item;
-  items.forEach((item: { guid: string; title: string; link: string; pubDate: string; description: string; source: string }) => {
-    res.push({
-      id: item.guid || "",
-      title: item.title || "",
-      link: item.link || "",
-      publicationDate: item.pubDate || "",
-      description: item.description || "",
-      source: item.source || ""
-    })
-  })
+  items.forEach(
+    (item: {
+      guid: string;
+      title: string;
+      link: string;
+      pubDate: string;
+      description: string;
+      source: string;
+    }) => {
+      res.push({
+        id: item.guid || "",
+        title: item.title || "",
+        link: item.link || "",
+        publicationDate: item.pubDate || "",
+        description: item.description || "",
+        source: item.source || "",
+      });
+    }
+  );
 
   return c.json({
-    items: res
-  })
-})
+    items: res,
+  });
+});
 
-export const GET = handle(app)
+app.get("/query/rss", async (c) => {
+  let { results } = await process.env.DB.prepare("SELECT * FROM rss").all();
+  return c.json(results);
+});
+
+export const GET = handle(app);
